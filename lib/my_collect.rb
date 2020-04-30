8f4775dde92b2e5673d1fef36d3031222f562f7a
Pat McClernan <patmccler@gmail.com>
2020-04-30 09:49:47 -0400

    build out my collect

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect collection
+  new_collection = []
+  
+  counter = block_given? ? 0 : array.size
+  while counter < collection.size
+    new_collection << yield(collection[counter])
+    counter += 1
+  end
+  new_collection
+end
 
 
