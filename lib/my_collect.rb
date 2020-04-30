10f6ecb457e0afb677aba7c3fb7bbc1cc9ddf0b0
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-04-30 11:17:05 -0400

    completed lab

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+def my_collect(arr)
+    collection = []
+    for i in 0..arr.length - 1
+        collection << yield(arr[i])
+    end
+    collection
+end
 
-
+# arr = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# p my_collect(arr){ |v| v.split(" ").first }
